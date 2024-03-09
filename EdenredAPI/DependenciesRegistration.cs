706551e75bf846e2b354bdbc14898736a6f8ecf5
Asad <asad.ashrafee@gmail.com>
2024-03-09 16:44:51 +0500

    Add project files.

---
+++ EdenredAPI/DependenciesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Edenred.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdenredAPI
+{
+    public static class DependenciesRegistration
+    {
+        public static void RegisterDependencies(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<EdenredDbContext>(e => e.UseSqlServer(Configuration.GetConnectionString("TechStoreConnection")));
+
+            //Services
+
+            //Repositories
+        }
+    }
+}
